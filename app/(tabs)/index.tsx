e6b2072862f6642250cef423cf64c428a6da7dbc
Jean Desauw <78590821+JeanDes-Code@users.noreply.github.com>
2025-10-23 09:41:02 +0200

    Uniwind update issue on web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,42 +1,9 @@
-import { View } from 'react-native';
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+import { Text, View } from 'react-native';
 
 export default function TabOneScreen() {
-  const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
-
-  const panGesture = Gesture.Pan()
-    .onUpdate((event) => {
-      translateX.value = event.translationX;
-      translateY.value = event.translationY;
-    })
-    .onEnd(() => {
-      translateX.value = withSpring(0);
-      translateY.value = withSpring(0);
-    });
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: translateX.value },
-        { translateY: translateY.value },
-      ],
-      // Tried using or not using className: same result on web.
-      // width: 100,
-      // height: 100,
-      // backgroundColor: 'blue',
-    };
-  });
-
   return (
-    <View className='flex-1 items-center justify-center bg-gray-900'>
-      <GestureDetector gesture={panGesture}>
-        <Animated.View 
-          style={animatedStyle}
-          className='w-16 h-16 bg-blue-500 rounded-lg'
-        />
-      </GestureDetector>
+    <View className='flex-1 items-center justify-center bg-cyan-900'>
+      <Text className='text-white text-4xl font-bold underline'>Hello World</Text>
     </View>
   );
 }
---
+++ components/TestingRNGH.tsx
@@ -0,0 +1,38 @@
+import { View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
+export default function TestingRNGH() {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const panGesture = Gesture.Pan()
+    .onUpdate((event) => {
+      translateX.value = event.translationX;
+      translateY.value = event.translationY;
+    })
+    .onEnd(() => {
+      translateX.value = withSpring(0);
+      translateY.value = withSpring(0);
+    });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
+  return (
+    <View className='flex-1 items-center justify-center bg-gray-900'>
+      <GestureDetector gesture={panGesture}>
+        <Animated.View 
+          style={animatedStyle}
+          className='w-16 h-16 bg-blue-500 rounded-lg'
+        />
+      </GestureDetector>
+    </View>
+  );
+}
